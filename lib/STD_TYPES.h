2c6248a6f9630b3df0c14f9470d3601802ea5b2a
AtefAlkabani <atefkabani@gmail.com>
2024-03-25 01:08:11 +0300

    version 4

--- lib/STD_TYPES.h
+++ lib/STD_TYPES.h
@@ -10,13 +10,20 @@
 
 typedef unsigned char 		u8 ;
 typedef signed char 		s8 ;
+
 typedef unsigned short int 	u16 ;
 typedef signed short int 	s16 ;
+
 typedef unsigned long int 	u32 ;
 typedef signed long int 	u32 ;
+
 typedef float			 	f32 ;
 typedef double				f64 ;
 
+typedef enum{
+	FALSE ,
+	TRUE
+}BOOL_t;
 
 
 #endif /* STD_TYPES_H_ */
